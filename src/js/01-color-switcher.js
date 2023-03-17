990551cbdb96815d110775cc322859087fd05a20
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-03-17 15:02:13 +0200

    First Task Done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,55 @@
+
+// Вихідні дані
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+stopBtn.disabled = true;
+
+// Функція для зміни атрибута на кнопці
+function changeBtnAttribute() 
+{
+  if (!startBtn.disabled) {
+    onChangeDisabled(true, false);
+  } else {
+    onChangeDisabled(false, true);
+  }
+}
+
+// Функція зміни стану кнопки
+function onChangeDisabled(startState, endState) 
+{
+  startBtn.disabled = startState;
+  stopBtn.disabled = endState;
+}
+
+// Функція зміни кольору
+function getRandomHexColor() 
+{
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Функція запуску зміни кольору
+function startChangeColor() 
+{
+  // для зміни кольору одразу за натискання кнопки. 
+  getRandomHexColor();
+
+  // Змінюємо кожну секунду
+  timerId = setInterval(() => 
+  {
+    getRandomHexColor();
+  }, 1000);
+
+  changeBtnAttribute();
+}
+
+// Функція для зупинки зміни кольору
+function stopChangingColor() 
+{
+  clearInterval(timerId);
+  changeBtnAttribute();
+}
+
+// Event handlers
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangingColor);
